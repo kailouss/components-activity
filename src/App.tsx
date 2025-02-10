51dc26b14de519b99282271191a78d6bfe73979b
kailouss <kaizensomosera.04@gmail.com>
2025-02-10 22:52:42 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from "./components/header/Header";
+import AboutPage from "./pages/AboutPage";
+import ContentsPage from "./pages/ContentsPage";
+import LandingPage from "./pages/LandingPage";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AnimatePresence mode="sync" onExitComplete={() => window.scrollTo(0, 0)}>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/contents" element={<ContentsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import Texts from "../components/container/Texts";
+
+const LandingPage = () => {
+  return (
+    <>
+      <motion.div
+        className="z-20 fixed inset-0 text-black h-screen overflow-hidden"
+        initial={{ x: "-100%", opacity: 1 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: "100%", opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="absolute text-white border-black bg-black flex-col md:clip-path-slanted-left h-full md:w-2/3 w-full items-center flex justify-center md:items-start md:p-5 gap-y-4">
+          <h1 className="text-5xl font-lexend font-bold">HELLO</h1>
+          <p>Software Components</p>
+          <Link to="/contents">
+            <button className="border w-auto h-10 bg-blue-500 p-2 rounded-2xl hover:bg-blue-700">
+              Get Started+
+            </button>
+          </Link>
+        </div>
+      </motion.div>
+      <motion.div
+        className="fixed inset-0 text-black h-screen"
+        initial={{ x: "100%", opacity: 1 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: "-100%", opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="z-30 flex justify-end items-center bg-white h-screen text-black text-5xl">
+          <Texts />
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default LandingPage;
